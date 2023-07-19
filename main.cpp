92ff063afe1c4e4fda29fe9552ae3cb728137d1b
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-07-19 19:35:18 -0400

    cmake setup

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <string>
+#include <thread>
 
 int main() {
     std::cout << "Welcome to tinytrade!";
     std::cout << "A simple stock exchange simulator.";
     return 0;
 }
+
--- websocket/port.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Welcome to tinytrade!";
-    std::cout << "Send your orders from me here";
-    return 0;
-}
