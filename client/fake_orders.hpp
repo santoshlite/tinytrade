cff95aa79427332727ffc9ee39cee679032a206d
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-08-18 01:27:55 -0400

    fake client limit orders complete

--- client/fake_orders.hpp
+++ client/fake_orders.hpp
@@ -2,11 +2,14 @@
 
 #include <iostream>
 #include <random>
+#include <string>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/lexical_cast.hpp>
-#include <random>
 #include "orders.pb.h"
+
+std::string gen_uuid();
+order_types::LimitOrder build_an_order();
 
 std::string gen_uuid(){
     std::string uuid_str = boost::lexical_cast<std::string>(boost::uuids::random_generator()());
@@ -15,14 +18,22 @@ std::string gen_uuid(){
 
 order_types::LimitOrder build_an_order(){
   std::random_device rd;
+  std::mt19937 generator(rd()); 
   std::uniform_int_distribution<int> dist(10000, 10000000);
-  order_types::LimitOrder order;
+  std::uniform_real_distribution<double> price_dist(9.85, 10.25); // fake price
+  order_types::LimitOrder order; 
   order.set_order_id(gen_uuid());
-  order.set_user_id(1232);
+  order.set_user_id(dist(generator)%10000);
   order.set_stock_id(999999999); // fixed
-  order.set_order_type(order_types::OrderType::BUY);
-  order.set_num_shares(10000);
-  order.set_partial_fill(true);
-  order.set_expire_time(1000);
+  double price = std::round(price_dist(generator)*100)/100;
+  order.set_unit_price(price);
+  if (dist(generator)%2 == 0) {
+        order.set_order_type(order_types::OrderType::BUY);
+  } else {
+        order.set_order_type(order_types::OrderType::SELL);
+  }
+  order.set_num_shares(dist(generator)%10000000+10000);
+  order.set_partial_fill(dist(generator)%2);
+  order.set_expire_time(dist(generator)%1440+10);
   return order;
 }
--- client/main.cpp
+++ client/main.cpp
@@ -7,7 +7,15 @@
 int main() {
     std::cout << "Welcome to tinytrade client!\n";
     for(int i = 0; i < 10; i++){
-        std::cout << gen_uuid() << "\n";
+        order_types::LimitOrder dummy_order = build_an_order();
+        std::cout << "== ORDER DETAILS =="<<"\n";
+        std::cout << "ORDER ID: " << dummy_order.order_id() << "\n";
+        std::cout << "USER ID: " << dummy_order.user_id() << "\n";
+        std::cout << "ORDER TYPE: " << dummy_order.order_type() << "\n";
+        std::cout << "UNIT PRICE: " << dummy_order.unit_price() << "\n";
+        std::cout << "NUM SHARES: " << dummy_order.num_shares() << "\n";
+        std::cout << "PARTIAL FILLS: " << dummy_order.partial_fill() << "\n";
+        std::cout << "EXPIRE TIME: " << dummy_order.expire_time() << "\n";
     }
     return 0;
 }
