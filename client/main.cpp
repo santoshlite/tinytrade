917b463ec35a44b284d2bf0b7ee8dfdec2097d19
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-08-20 01:39:44 -0400

    reading from lobster enabled + send()

--- client/main.cpp
+++ client/main.cpp
@@ -3,42 +3,62 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 #include <ios>
 #include "send.hpp"
 #include "orders.pb.h"
 
 int main() {
     std::cout << "Welcome to tinytrade client!\n";
-    std::fstream fin;
 
-    fin.open("fake_data.csv", std::ios::in);
+    std::ifstream fin;
 
-    std::vector<std::vector<std::string>> rows;
+    fin.open("../../client/fake_data.csv", std::ios::in);
+
+    if (!fin.is_open()) {
+        std::cerr << "Error opening file." << std::endl;
+    }
+
     std::vector<std::string> row;
-    std::string line, word, temp;
-    while (fin >> temp) {
+    std::string line, word;
+    while (getline(fin, line)) {
+        row.clear();
+        word.clear();
+        std::stringstream iss(line);
+        while(getline(iss, word, ',')){
+            row.push_back(word);
+        }
+        orders::Message m;
+        m.set_order_id(static_cast<uint32_t>(std::stoul(row[0])));
+        m.set_user_id(static_cast<uint32_t>(std::stoul(row[1])));
+        m.set_stock_id(static_cast<uint32_t>(std::stoul(row[2])));
+        m.set_event_type(static_cast<uint32_t>(std::stoul(row[3])));
+        m.set_num_shares(static_cast<uint32_t>(std::stoul(row[4])));
+        m.set_unit_price(static_cast<uint32_t>(std::stoul(row[5])));
+        m.set_order_type(stoi(row[6]));
+        send(m);
+    }
+        /*
         row.clear();
         getline(fin, line);
         std::stringstream s(line);
         while (getline(s, word, ',')){
             row.push_back(word);
         }
-
+        std::cout << "ROW: " << row[0] << std::endl;
         orders::Message m;
         m.set_order_id(stoi(row[0]));
-        //m.set_user_id(row[1]);
-        //m.order_id = row[0];
-        //m.user_id = row[1];
-        //m.stock_id = row[2];
-        //m.event_type = row[3];
-        //m.unit_price = row[4];
-        //m.num_shares = row[5];
-        //m.order_type = row[6];
-
+        m.set_user_id(stoi(row[1]));
+        m.set_order_id(stoi(row[2]));
+        m.set_user_id(stoi(row[3]));
+        m.set_stock_id(stoi(row[4]));
+        m.set_event_type(stoi(row[5]));
+        m.set_unit_price(stoi(row[6]));
+        m.set_num_shares(stoi(row[7]));
+        m.set_order_type(stoi(row[8]));
         send(m);
-
-    }
-
+        */
+    fin.close();
     return 0;
 }
 
--- client/send.hpp
+++ client/send.hpp
@@ -2,10 +2,10 @@
 #include <string>
 #include "orders.pb.h"
 
-std::string send(orders::Message m);
+void send(orders::Message m);
 
-std::string send(orders::Message m){
-        std::cout << "== ORDER DETAILS =="<<"\n";
+void send(orders::Message m){
+        std::cout << "== MESSAGE DETAILS =="<<"\n";
         std::cout << "ORDER ID: " << m.order_id() << "\n";
         std::cout << "USER ID: " << m.user_id() << "\n";
         std::cout << "STOCK ID: " << m.stock_id() << "\n";
@@ -13,5 +13,4 @@ std::string send(orders::Message m){
         std::cout << "UNIT PRICE: " << m.unit_price() << "\n";
         std::cout << "NUM SHARES: " << m.num_shares() << "\n";
         std::cout << "ORDER TYPE: " << m.order_type() << "\n";
-
 }
