6d8be51b25aa156b97214bbe918132402e3fe969
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-08-21 22:41:57 -0400

    use protobuf arena

--- client/main.cpp
+++ client/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <filesystem>
 #include <ios>
+#include <google/protobuf/arena.h>
 #include "send.hpp"
 #include "orders.pb.h"
 
@@ -18,7 +19,8 @@ int main() {
     if (!fin.is_open()) {
         std::cerr << "Error opening file." << std::endl;
     }
-
+    
+    google::protobuf::Arena arena;
     std::vector<std::string> row;
     std::string line, word;
     while (getline(fin, line)) {
@@ -28,15 +30,17 @@ int main() {
         while(getline(iss, word, ',')){
             row.push_back(word);
         }
-        orders::Message m;
-        m.set_order_id(static_cast<uint32_t>(std::stoul(row[0])));
-        m.set_user_id(static_cast<uint32_t>(std::stoul(row[1])));
-        m.set_stock_id(static_cast<uint32_t>(std::stoul(row[2])));
-        m.set_event_type(static_cast<uint32_t>(std::stoul(row[3])));
-        m.set_num_shares(static_cast<uint32_t>(std::stoul(row[4])));
-        m.set_unit_price(static_cast<uint32_t>(std::stoul(row[5])));
-        m.set_order_type(stoi(row[6]));
-        send(m);
+        orders::Message* arena_message = google::protobuf::Arena::CreateMessage<orders::Message>(&arena);
+        arena_message->set_order_id(static_cast<uint32_t>(std::stoul(row[0])));
+        arena_message->set_user_id(static_cast<uint32_t>(std::stoul(row[1])));
+        arena_message->set_stock_id(static_cast<uint32_t>(std::stoul(row[2])));
+        arena_message->set_event_type(static_cast<uint32_t>(std::stoul(row[3])));
+        arena_message->set_num_shares(static_cast<uint32_t>(std::stoul(row[4])));
+        arena_message->set_unit_price(static_cast<uint32_t>(std::stoul(row[5])));
+        arena_message->set_order_type(stoi(row[6]));
+        if(arena_message->IsInitialized()){
+            send(arena_message);
+        }
     }
         /*
         row.clear();
--- client/send.hpp
+++ client/send.hpp
@@ -4,13 +4,7 @@
 
 void send(orders::Message m);
 
-void send(orders::Message m){
+void send(orders::Message* m){
         std::cout << "== MESSAGE DETAILS =="<<"\n";
-        std::cout << "ORDER ID: " << m.order_id() << "\n";
-        std::cout << "USER ID: " << m.user_id() << "\n";
-        std::cout << "STOCK ID: " << m.stock_id() << "\n";
-        std::cout << "EVENT TYPE: " << m.event_type() << "\n";
-        std::cout << "UNIT PRICE: " << m.unit_price() << "\n";
-        std::cout << "NUM SHARES: " << m.num_shares() << "\n";
-        std::cout << "ORDER TYPE: " << m.order_type() << "\n";
+        std::cout << m->DebugString() << "\n";
 }
